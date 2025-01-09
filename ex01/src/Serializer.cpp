ecc7f4f9a77dfab5c528fc61d3ce31d6558a023b
yacoub cherouali <cheroualiy@gmail.com>
2025-01-09 15:16:19 +0100

    serializer

---
+++ ex01/src/Serializer.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ycheroua <ycheroua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 17:37:44 by ycheroua          #+#    #+#             */
+/*   Updated: 2025/01/08 15:41:19 by ycheroua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "Serializer.hpp"
+
+Serializer::Serializer(void) {}
+Serializer::Serializer(const Serializer  &copy) {(void)copy;}
+Serializer::~Serializer() {}
+Serializer& Serializer::operator=(const Serializer& copy) 
+{
+	(void)copy;
+	return *this;
+}
+
+uintptr_t Serializer::serialize(Data *ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data *Serializer::deserialize(uintptr_t raw)
+{
+	return reinterpret_cast<Data*>(raw);
+}
+
+
+// Data2 *Serializer::deserialize2(uintptr_t raw)
+// {
+// 	return reinterpret_cast<Data2*>(raw);
+// }
+
+
+// Data2 *Serializer::deserializeData(Data* data)
+// {
+// 	return reinterpret_cast<Data2*>(data);
+// }
+
+
---
+++ ex01/src/Serializer.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ycheroua <ycheroua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 17:37:46 by ycheroua          #+#    #+#             */
+/*   Updated: 2025/01/09 15:13:40 by ycheroua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <limits>
+#include <string>
+#include <iomanip>
+
+
+// // for testing 
+// struct Data2 {
+//     int id;
+//     std::string name;
+//     float value;
+// };
+
+
+struct Data {
+    int id;
+    std::string name;
+    float value;
+};
+
+class Serializer
+{
+	private:
+		Serializer(void);
+		Serializer(const Serializer &copy);
+		virtual ~Serializer(void);
+		Serializer& operator=(const Serializer &copy);
+		
+	public:
+		static uintptr_t serialize(Data* ptr);
+		static Data* deserialize(uintptr_t raw);
+		// static Data2* deserialize2(uintptr_t raw);
+		// static Data2* deserializeData(Data* data);
+
+};
+
+#endif
